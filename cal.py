14c8a74ff055354fc23a5177c8f7b5239ab31c4f
Alexander Bauer <sasha@linux.com>
2015-12-08 14:50:14 -0500

    Add dict-based constructors for JSON parsing

--- cal.py
+++ cal.py
@@ -4,6 +4,10 @@ import datetime
 ICAL_PRODID = 'https://github.com/AlexanderBauer/deschedule'
 ICAL_VERSION = '2.0'
 
+JSON_DATEF = '%Y-%m-%d'
+JSON_TIMEF = '%H:%M'
+JSON_WEEKDAYS = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+
 class Semester:
     def __init__(self, start, end, classes, breaks=[], holidays=[]):
         self.start = start
@@ -42,6 +46,17 @@ class Semester:
 
         return cal
 
+    @classmethod
+    def from_json_dict(cls, d):
+        args = d
+        args['start']   = datetime.datetime.strptime(d['start'],
+                JSON_DATEF).date()
+        args['end']   = datetime.datetime.strptime(d['end'],
+                JSON_DATEF).date()
+        args['classes'] = {name: Class.from_json_dict(properties) for name,
+                   properties in d['classes'].items()}
+        return cls(**args)
+
 class Class:
     def __init__(self, sections):
         self.sections = sections
@@ -50,10 +65,17 @@ class Class:
         return {section_name: map(section.time_duration, filter(section.on_day, days))
                 for section_name, section in self.sections.items()}
 
+    @classmethod
+    def from_json_dict(cls, d):
+        args = d
+        args['sections'] = {kind: Section.from_json_dict(properties)
+                for kind, properties in d['sections'].items()}
+        return cls(**args)
+
 class Section:
     def __init__(self, days, time, length):
-        self.days = days
-        self.time = time
+        self.days   = days
+        self.time   = time
         self.length = length
 
     def on_day(self, date):
@@ -61,3 +83,11 @@ class Section:
 
     def time_duration(self, date):
         return (datetime.datetime.combine(date, self.time), self.length)
+
+    @classmethod
+    def from_json_dict(cls, d):
+        args = d
+        args['days']   = [JSON_WEEKDAYS.index(day) for day in d['days']]
+        args['time']   = datetime.datetime.strptime(d['time'], JSON_TIMEF).time()
+        args['length'] = datetime.timedelta(minutes=d['length'])
+        return cls(**args)
--- test.py
+++ test.py
@@ -3,18 +3,21 @@
 import cal
 import datetime
 
-s = cal.Semester(
-    start   = datetime.date(2016, 1, 25),
-    end     = datetime.date(2016, 5, 10),
-    classes = {
-      'ABCD 101': cal.Class({
-        'Lecture': cal.Section(
-          days   = [ 2, 4 ],
-          time   = datetime.time(13, 0),
-          length = datetime.timedelta(minutes=75))
-        })
-    })
-
+s = cal.Semester.from_json_dict({
+    'start':   '2016-01-25',
+    'end':     '2016-05-10',
+    'classes': {
+        'ABCD 101': {
+            'sections': {
+                'Lecture': {
+                    'days':   [ 'Tue', 'Thu' ],
+                    'time':   '13:00',
+                    'length': 75
+                }
+            }
+        }
+    }
+})
 print(s.calendar())
 
 with open('test.ical', 'wb') as f:
