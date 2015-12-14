fe140b5e99032c731a05de3464f27f5ce6aa21d0
Alexander Bauer <sasha@linux.com>
2015-12-14 14:24:36 -0500

    Add basic styling for section results

---
+++ app/static/style.js
@@ -0,0 +1,33 @@
+function style_section(section) {
+  return '<div class="section mui-panel">' +
+           '<div class="section-header mui-row">' +
+             '<div class="mui-col-md-4">' +
+               '<span class="section-class-code mui--align-middle">' + section.class_code + '</span>' +
+               '<span class="section-kind mui--align-middle">' + section.kind + '</span>' +
+               '<span class="section-number mui--align-middle">' + section.number + '</span>' +
+             '</div>' +
+             '<div class="mui-col-md-4 mui-col-md-offset-4">' +
+               '<span class="section-instructor mui--align-middle">' + section.instructor + '</span>' +
+             '</div>' +
+           '</div>' +
+           '<div class="section-info mui-row">' +
+             '<div class="mui-col-md-4">' +
+               '<span class="section-time mui--align-middle">' +
+                 section.time +
+               '</span>' +
+             '</div>' +
+             '<div class="mui-col-md-4">' +
+               '<span class="section-room mui--align-middle">' +
+                 (section.days.join('/')) +
+               '</span>' +
+             '</div>' +
+             '<div class="mui-col-md-4">' +
+               '<span class="section-room mui--align-middle">' +
+                 ((section.room != null) ? section.room : '') +
+               '</span>' +
+             '</div>' +
+           '</div>' +
+         '</div>';
+}
+
+console.log('load');
