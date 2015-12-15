3e664c0b4f101aaf7da57accd6ae4d0a9ff8a9d3
Alexander Bauer <sasha@linux.com>
2015-12-15 00:53:46 -0500

    Allow binding to other hosts and ports

--- run.py
+++ run.py
@@ -13,7 +13,7 @@ def main(args):
         from app import db
         db.create_all()
 
-    app.run(debug=args.debug)
+    app.run(host=args.host, port=args.port, debug=args.debug)
 
 def parse(args):
     parser = argparse.ArgumentParser()
@@ -21,8 +21,11 @@ def parse(args):
     parser.add_argument('--db', default='sqlite:///' + os.path.join(basedir, 'app.db'))
     parser.add_argument('--migrate', default=os.path.join(basedir, 'db_repository'))
     parser.add_argument('--create', action='store_false', default=True)
+    parser.add_argument('--host', default='0.0.0.0')
+    parser.add_argument('--port', type=int, default=5000)
     parser.add_argument('--debug', action='store_true', default=False)
     return parser.parse_args(args)
 
 if __name__ == '__main__':
     sys.exit(main(parse(sys.argv[1:])))
+
