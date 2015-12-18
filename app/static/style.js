69680884c1a49ba326cf74239dc71046485a0b1c
Alexander Bauer <sasha@linux.com>
2015-12-17 23:43:02 -0500

    Clean up section card headers

--- app/static/style.js
+++ app/static/style.js
@@ -7,11 +7,16 @@ function style_section(section) {
            '<div class="section card waves-effect waves-block waves-light" data-classcode="' +
               section.class_code + '" data-number="' + section.number + '">' +
              '<div class="card-content">' +
-               '<div class="section-header card-title">' +
-                 section.class_code + ' ' + section.number + ' ' + section.kind +
+               '<div class="section-header card-title row">' +
+                 '<div class="col s6 left-align">' +
+                   section.class_code + ' ' + section.kind + ' ' +
+                   '<span class="thin section-number">' + section.number + '</span>' +
+                 '</div>' +
+                 '<div class="col s6 right-align">' +
+                   section.instructor +
+                 '</div>' +
                '</div>' +
                '<ul class="section-info">' +
-                 '<li>' + section.instructor + '</li>' +
                  '<li>' + section.time + '</li>' +
                  '<li>' + (section.days.join('/')) + '</li>' +
                  '<li>' + ((section.room != null) ? section.room : '') + '</li>' +
