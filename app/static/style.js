bc6bb27da0ed61c6fa8baa288e91835ed90edece
Alexander Bauer <sasha@linux.com>
2015-12-17 20:58:24 -0500

    Restyle cards partially

--- app/static/style.js
+++ app/static/style.js
@@ -3,34 +3,18 @@ function style_semester_li(semester) {
 }
 
 function style_section(section) {
-  return '<div class="mui-panel section" data-classcode="' +
+  return '<div class="card" data-classcode="' +
              section.class_code + '" data-number="' + section.number + '">' +
-           '<div class="section-header mui-row">' +
-             '<div class="mui-col-md-4">' +
-               '<span class="section-class-code mui--align-middle">' + section.class_code + '</span>' +
-               '<span class="section-kind mui--align-middle">' + section.kind + '</span>' +
-               '<span class="section-number mui--align-middle">' + section.number + '</span>' +
+           '<div class="card-content">' +
+             '<div class="section-header card-title">' +
+               section.class_code + ' ' + section.number + ' ' + section.kind +
              '</div>' +
-             '<div class="mui-col-md-4 mui-col-md-offset-4">' +
-               '<span class="section-instructor mui--align-middle">' + section.instructor + '</span>' +
-             '</div>' +
-           '</div>' +
-           '<div class="section-info mui-row">' +
-             '<div class="mui-col-md-4">' +
-               '<span class="section-time mui--align-middle">' +
-                 section.time +
-               '</span>' +
-             '</div>' +
-             '<div class="mui-col-md-4">' +
-               '<span class="section-room mui--align-middle">' +
-                 (section.days.join('/')) +
-               '</span>' +
-             '</div>' +
-             '<div class="mui-col-md-4">' +
-               '<span class="section-room mui--align-middle">' +
-                 ((section.room != null) ? section.room : '') +
-               '</span>' +
-             '</div>' +
+             '<ul class="section-info">' +
+               '<li>' + section.instructor + '</li>' +
+               '<li>' + section.time + '</li>' +
+               '<li>' + (section.days.join('/')) + '</li>' +
+               '<li>' + ((section.room != null) ? section.room : '') + '</li>' +
+             '</ul>' +
            '</div>' +
          '</div>';
 }
